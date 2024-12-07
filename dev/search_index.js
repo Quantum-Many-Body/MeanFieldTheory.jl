0156904e57348e7ca96ed27f3fab3a4a27307caa
Documenter.jl <documenter@juliadocs.github.io>
2024-12-07 03:26:49 +0000

    build based on 545e241

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MeanFieldTheory","category":"page"},{"location":"#MeanFieldTheory","page":"Home","title":"MeanFieldTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MeanFieldTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MeanFieldTheory]","category":"page"}]
+}
