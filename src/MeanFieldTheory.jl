47da5cf1aff50e4f8cf30de5f0a167ae150abf39
waltergu <waltergu1989@gmail.com>
2025-07-21 16:43:31 +0800

    First working version.

--- src/MeanFieldTheory.jl
+++ src/MeanFieldTheory.jl
@@ -1,59 +1,101 @@
 module MeanFieldTheory
 
-using ChainRulesCore: HasReverseMode, NoTangent, RuleConfig, rrule_via_ad
 using Distributed: @distributed
+using HCubature: hcubature
 using LinearAlgebra: eigvals
-using QuantumLattices: BrillouinZone, kind, update
+using Optim: LBFGS, Options, optimize
+using QuantumLattices: Algorithm, BrillouinZone, expand, iscontinuous, isdiscrete, kind, periods, update
+using StaticArrays: SVector
 using TightBindingApproximation: CompositeTBA, Fermionic, SimpleTBA, TBA, TBAKind
 
-import ChainRulesCore: rrule
 import QuantumLattices: Parameters, dimension, matrix, scalartype, update!
+import TightBindingApproximation.Fitting: optimize!
 
-export SCMF, Ω, constant
+export SCMF, Ω, Ω₀, Ω₀!, constant, normal, meanfield, optimize!
 
+"""
+    const PureTBA{K<:TBAKind} = Union{SimpleTBA{K}, CompositeTBA{K}}
+
+Pure tight-binding-approximation, type alias for `Union{SimpleTBA{K}, CompositeTBA{K}}`.
+"""
 const PureTBA{K<:TBAKind} = Union{SimpleTBA{K}, CompositeTBA{K}}
 
-mutable struct SCMF{K<:Fermionic, F<:PureTBA{K}, I<:Parameters, M<:PureTBA{K}, C<:Function, B<:BrillouinZone} <: TBA{K, F, Nothing}
+"""
+    SCMF{K<:Fermionic, N<:PureTBA{K}, B<:BrillouinZone, I<:Parameters, M<:PureTBA{K}, C<:Function} <: TBA{K, N, Nothing}
+
+Self-consistent mean-field theory for fermionic systems.
+"""
+mutable struct SCMF{K<:Fermionic, N<:PureTBA{K}, B<:BrillouinZone, I<:Parameters, M<:PureTBA{K}, C<:Function} <: TBA{K, N, Nothing}
     T::Float64
-    const free::F
+    const normal::N
     interactions::I
-    const meanfields::M
+    const meanfield::M
     const constant::C
     const brillouinzone::B
-    function SCMF(T::Real, free::PureTBA, interactions::Parameters, meanfields::TBA, constant::Function, brillouinzone::BrillouinZone)
-        @assert kind(free)==kind(meanfields) && free.lattice==meanfields.lattice && dimension(free)==dimension(meanfields) "SCMF error: mismatched free part and meanfields part."
-        new{typeof(kind(free)), typeof(free), typeof(interactions), typeof(meanfields), typeof(constant), typeof(brillouinzone)}(T, free, interactions, meanfields, constant, brillouinzone)
+    function SCMF(T::Real, normal::PureTBA, interactions::Parameters, meanfield::PureTBA, constant::Function, brillouinzone::BrillouinZone)
+        @assert kind(normal)==kind(meanfield) && normal.lattice==meanfield.lattice && dimension(normal)==dimension(meanfield) "SCMF error: mismatched normal part and mean-field part."
+        @assert iscontinuous(brillouinzone) || isdiscrete(brillouinzone) "SCMF error: the input brillouin zone is neither continuous nor discrete."
+        new{typeof(kind(normal)), typeof(normal), typeof(brillouinzone), typeof(interactions), typeof(meanfield), typeof(constant)}(T, normal, interactions, meanfield, constant, brillouinzone)
     end
 end
-@inline scalartype(::Type{<:SCMF{<:Fermionic, F, M}}) where {F<:PureTBA, M<:PureTBA} = promote_type(scalartype(F), scalartype(M))
-@inline Parameters(scmf::SCMF) = (; T=scmf.T, Parameters(scmf.free)..., scmf.interactions..., Parameters(scmf.meanfields)...)
+@inline scalartype(::Type{<:SCMF{<:Fermionic, N, M}}) where {N<:PureTBA, M<:PureTBA} = promote_type(scalartype(N), scalartype(M))
+@inline Parameters(scmf::SCMF) = (; T=scmf.T, Parameters(scmf.normal)..., scmf.interactions..., Parameters(scmf.meanfield)...)
 @inline function update!(scmf::SCMF; parameters...)
     if length(parameters)>0
         scmf.T = get(parameters, :T, scmf.T)
-        update!(scmf.free; parameters...)
+        update!(scmf.normal; parameters...)
         scmf.interactions = update(scmf.interactions; parameters...)
-        update!(scmf.meanfields; parameters...)
+        update!(scmf.meanfield; parameters...)
     end
     return scmf
 end
-@inline dimension(scmf::SCMF) = dimension(scmf.free)
+@inline dimension(scmf::SCMF) = dimension(scmf.normal)
 @inline function matrix(scmf::SCMF, k::Union{AbstractVector{<:Number}, Nothing}=nothing; kwargs...)
-    m₁ = matrix(scmf.free, k; kwargs...)
-    m₂ = matrix(scmf.meanfields, k; kwargs...)
+    m₁ = matrix(scmf.normal, k; kwargs...)
+    m₂ = matrix(scmf.meanfield, k; kwargs...)
     return m₁ + m₂
-end
-@inline function constant(scmf::SCMF)
-    return scmf.constant(values(scmf.interactions)..., values(Parameters(scmf.meanfields))...)
 end
 
 """
+    normal(scmf::SCMF) -> TBA
+    normal(scmf::Algorithm{<:SCMF}) -> TBA
+
+Get the normal part.
 """
-function (scmf::SCMF)(vs::Number...; kwargs...)
-    update!(scmf; Parameters{keys(Parameters(scmf.meanfields))}(vs...)...)
+@inline normal(scmf::SCMF) = scmf.normal
+@inline normal(scmf::Algorithm{<:SCMF}) = normal(scmf.frontend)
+
+"""
+    meanfield(scmf::SCMF) -> TBA
+    meanfield(scmf::Algorithm{<:SCMF}) -> TBA
+
+Get the mean-field part.
+"""
+@inline meanfield(scmf::SCMF) = scmf.meanfield
+@inline meanfield(scmf::Algorithm{<:SCMF}) = meanfield(scmf.frontend)
+
+"""
+    constant(scmf::SCMF) -> Real
+    constant(scmf::Algorithm{<:SCMF}) -> Real
+
+Get the constant part of the free energy of a fermionic system at the mean-field level.
+"""
+@inline constant(scmf::Algorithm{<:SCMF}) = constant(scmf.frontend)
+@inline constant(scmf::SCMF) = scmf.constant(values(scmf.interactions)..., values(Parameters(scmf.meanfield))...)
+
+"""
+    Ω(scmf::SCMF; kwargs...) -> Real
+    Ω(scmf::Algorithm{<:SCMF}; kwargs...) -> Real
+
+Get the free energy of a fermionic system at the mean-field level.
+"""
+@inline Ω(scmf::Algorithm{<:SCMF}; kwargs...) = Ω(scmf.frontend; kwargs...)
+@inline Ω(scmf::SCMF; kwargs...) = Ω_(Val(iscontinuous(scmf.brillouinzone)), scmf; kwargs...)
+function Ω_(::Val{false}, scmf::SCMF; kwargs...)
     result = (@distributed (+) for k in scmf.brillouinzone
         temp = 0.0
         for e in eigvals(scmf, k; kwargs...)
-            temp += Ω(e, scmf.T; kwargs...)
+            temp += Ω_(e, scmf.T; kwargs...)
         end
         temp
     end)::Float64
@@ -61,43 +103,20 @@ function (scmf::SCMF)(vs::Number...; kwargs...)
     result += constant(scmf)
     return result
 end
-
-function rrule(config::RuleConfig{>:HasReverseMode}, scmf::SCMF, vs::Number...; kwargs...)
-    vs = promote(vs...)
-    update!(scmf; Parameters{keys(Parameters(scmf.meanfields))}(vs...)...)
-    primal = 0.0
-    dvs = zeros(eltype(vs), length(vs))
-    Δes = zeros(eltype(vs), length(vs))
-    Δms = []
-    for k in scmf.brillouinzone
-        m = matrix(scmf, k; kwargs...)
-        es, es_pullback = rrule(eigvals, m)
-        for (i, e) in enumerate(es)
-            ω, ω_pullback = rrule(Ω, e, scmf.T; kwargs...)
-            primal += ω
-            Δes[i] = ω_pullback(1.0)[2]
+function Ω_(::Val{true}, scmf::SCMF; atol=10^-6, rtol=10^-6, kwargs...)
+    function integrand(k)
+        result = 0.0
+        for e in eigvals(scmf, expand(scmf.brillouinzone, k); kwargs...)
+            result += Ω_(e, scmf.T; kwargs...)
         end
-        Δm = es_pullback(Δes)
-        for i in eachindex(dvs)
-            dvs[i] += mapreduce(*, +, Δm, Δms[i])
-        end
+        return result
     end
-    primal /= length(scmf.brillouinzone)
-    for i in eachindex(dvs)
-        dvs[i] /= length(scmf.brillouinzone)
-    end
-    c, c_pullback = rrule_via_ad(config, scmf.constant, values(scmf.interactions)..., vs...)
-    primal += c
-    for (i, dc) in enumerate(c_pullback(1.0)[end-length(vs):end])
-        dvs[i] += Δc[index]
-    end
-    scmf_pullback(Δscmf) = (NoTangent(), ntuple(i->Δscmf*dvs[i], Val(fieldcount(typeof(vs))))...)
-    return primal, scmf_pullback
+    ps = periods(keytype(scmf.brillouinzone))
+    lower, upper = map(p->0.0, ps), map(p->1.0, ps)
+    result = hcubature(integrand, lower, upper; atol=atol, rtol=rtol)
+    return first(result) + constant(scmf)
 end
-
-"""
-"""
-function Ω(e::Real, T::Real; cut::Real=100.0, kwargs...)
+function Ω_(e::Real, T::Real; cut::Real=100.0, kwargs...)
     e = convert(Float64, e)
     T = convert(Float64, T)
     ratio = e/T
@@ -110,20 +129,53 @@ function Ω(e::Real, T::Real; cut::Real=100.0, kwargs...)
     end
     return result
 end
-function rrule(::typeof(Ω), e::Real, T::Real; cut::Real=100.0, kwargs...)
-    e = convert(Float64, e)
-    T = convert(Float64, T)
-    ratio = e/T
-    primal, de, dT = if abs(ratio)>cut
-        e>0 ? (zero(e), zero(e), zero(e)) : (e, one(e), zero(e))
-    else
-        x = exp(-ratio)
-        y = 1 + x
-        z = log(y)
-        (-T*z, x/y, -z-(ratio*x)/y)
+
+"""
+    Ω₀(scmf::SCMF; kwargs...) -> Real
+    Ω₀(scmf::Algorithm{<:SCMF}; kwargs...) -> Real
+
+Get the free energy of a fermionic system of the normal state at the mean-field level.
+"""
+@inline Ω₀(scmf::Algorithm{<:SCMF}; kwargs...) = Ω₀(scmf.frontend; kwargs...)
+function Ω₀(scmf::SCMF; kwargs...)
+    fields = Parameters(meanfield(scmf))
+    params = Parameters{keys(fields)}(map(v->zero(v), values(fields))...)
+    return Ω(update!(deepcopy(scmf); params...); kwargs...)
+end
+
+"""
+    optimize!(
+        scmf::Union{SCMF, Algorithm{<:SCMF}}, variables=keys(Parameters(meanfield(scmf)));
+        verbose=false,
+        method=LBFGS(),
+        options=Options(x_abstol=4*10^-6, f_abstol=4*10^-6, iterations=1000, show_trace=true),
+        condensation::Bool=false,
+        kwargs...
+    )
+
+Optimize the order parameters of a fermionic system by the self-consistent mean-field theory.
+"""
+function optimize!(
+    scmf::Union{SCMF, Algorithm{<:SCMF}}, variables=keys(Parameters(meanfield(scmf)));
+    verbose=false,
+    method=LBFGS(),
+    options=Options(x_abstol=4*10^-6, f_abstol=4*10^-6, iterations=1000, show_trace=true),
+    condensation::Bool=false,
+    kwargs...
+)
+    v₀ = collect(getfield(Parameters(scmf), name) for name in variables)
+    normal = condensation ? Ω₀(scmf; kwargs...) : 0.0
+    function objective(v::Vector)
+        parameters = Parameters{variables}(v...)
+        update!(scmf; parameters...)
+        verbose && println(parameters)
+        return Ω(scmf; kwargs...) - normal
     end
-    Ω_pullback(ΔΩ) = NoTangent(), ΔΩ*de, ΔΩ*dT
-    return primal, Ω_pullback
+    v₀ = collect(getfield(Parameters(scmf), name) for name in variables)
+    op = optimize(objective, v₀, method, options)
+    parameters = Parameters{variables}(op.minimizer...)
+    update!(scmf; parameters...)
+    return scmf, op
 end
 
 end
--- test/MeanFieldTheory.jl
+++ test/MeanFieldTheory.jl
@@ -1,22 +1,43 @@
 using MeanFieldTheory
-using QuantumLattices: Bond, BrillouinZone, Fock, Hilbert, Hopping, Lattice, Onsite, 𝕔⁺𝕔, reciprocals, @σ_str
-using TightBindingApproximation: Fermionic, TBA
+using QuantumLattices: Algorithm, Bond, BrillouinZone, Fock, Hilbert, Hopping, Lattice, Onsite, Parameters, 𝕔⁺𝕔, dimension, reciprocals, scalartype, update!, @σ_str
+using TightBindingApproximation: TBA
 
 @time @testset "MeanFieldTheory" begin
-
-    lattice = Lattice([0.0, 0.0], [1.0, 0.0]; vectors=[[1.0, 1.0], [1.0, -1.0]])
+    lattice = Lattice([0.0, 0.0], [0.0, √3/3]; vectors=[[1.0, 0.0], [0.5, √3/2]])
     hilbert = Hilbert(Fock{:f}(1, 2), length(lattice))
 
+    T = 0.0
     U = 3.0
     t = Hopping(:t, -1.0, 1)
-    m = Onsite(:m, 1.0, bond::Bond->iseven(first(bond).site) ? 𝕔⁺𝕔(:, :, σ"z", :) : 𝕔⁺𝕔(:, :, -σ"z", :))
-    constant(U, m) = m^2/U
+    m = Onsite(:m, 0.2, bond::Bond->iseven(first(bond).site) ? 𝕔⁺𝕔(:, :, σ"z", :) : 𝕔⁺𝕔(:, :, -σ"z", :))
 
-    # λ = Onsite(:λ, 0.0)
-    # constant(U, m, λ) = m^2/U - 2*λ
+    afm_normal = TBA(lattice, hilbert, t)
+    afm_interaction = (U=U,)
+    afm_meanfield = TBA(lattice, hilbert, m)
+    afm_constant = (U, m)->m^2/U
 
-    free = TBA(lattice, hilbert, t)
-    meanfields = TBA(lattice, hilbert, m)
-    brillouinzone = BrillouinZone(reciprocals(lattice), 100)
-    scmf = SCMF(0.0, free, (U=U,), meanfields, constant, brillouinzone)
+    scmf = Algorithm(:HoneycombAFM, SCMF(T, deepcopy(afm_normal), afm_interaction, deepcopy(afm_meanfield), afm_constant, BrillouinZone(reciprocals(lattice), 100)))
+    @test scalartype(scmf.frontend) == scalartype(typeof(scmf.frontend)) == Float64
+    @test Parameters(scmf.frontend) == (T=T, t=-1.0, U=U, m=0.2)
+    @test dimension(scmf.frontend) == 4
+    @test normal(scmf) == normal(scmf.frontend) == afm_normal
+    @test meanfield(scmf) == meanfield(scmf.frontend) == afm_meanfield
+    @test constant(scmf) == constant(scmf.frontend) == 0.2^2/3
+    @test Ω(scmf) ≈ Ω(scmf.frontend) ≈ -3.169759127781753
+    @test Ω₀(scmf) ≈ Ω₀(scmf.frontend) ≈ -3.1491956389539215
+    _, op = optimize!(scmf; condensation=true)
+    @test op.minimum ≈ -0.8006432056638455
+    @test op.minimizer ≈ [2.5343897118367074]
+
+    scmf = Algorithm(:HoneycombAFM, SCMF(T, deepcopy(afm_normal), afm_interaction, deepcopy(afm_meanfield), afm_constant, BrillouinZone(reciprocals(lattice), Inf)))
+    @test Ω(scmf) ≈ Ω(scmf.frontend) ≈ -3.1697591635049887
+    @test Ω₀(scmf) ≈ Ω₀(scmf.frontend) ≈ -3.149194521076783
+    _, op = optimize!(scmf; condensation=true)
+    @test op.minimum ≈ -0.8006443303090753
+    @test op.minimizer ≈ [2.5343897176328776]
+
+    update!(scmf; T=1.3)
+    _, op = optimize!(scmf; condensation=true)
+    @test op.minimum ≈ -0.0016329106484294087
+    @test op.minimizer ≈ [0.6975124223291923]
 end
